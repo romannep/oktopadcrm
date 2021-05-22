f814d53e2880fb2588d2b65573c54c51480b40eb
Roman Nep <roman.nep@electroneek.com>
2021-05-22 17:20:17 +0400

    udpates POC

--- src/AppServer.js
+++ src/AppServer.js
@@ -1,3 +1,4 @@
+import fs from 'fs';
 import { makeEntitiesFromStructures, use } from 'katejs';
 import { AppDoc, AppDocs, AppPrint, AppSettings, AppImport, AppUser } from 'katejs-modules';
 import AppTrigger from './katejs-trigger/lib/AppServer';
@@ -12,6 +13,7 @@ import Expense from './entities/Expense';
 import Receipt from './entities/Receipt';
 import DealComment from './entities/DealComment';
 
+import systemUpdates from './system-updates';
 
 const AppServer = parent => class Server extends
   use(parent, AppUser, AppDoc, AppPrint, AppDocs, AppSettings, AppImport, AppTrigger, AppFields, AppModules) {
@@ -43,6 +45,33 @@ const AppServer = parent => class Server extends
     this.modules = {
       school: () => require('./modules/school/Server'),
     };
+
+    this.systemUpdates = this.systemUpdates || [];
+    this.systemUpdates.push(...systemUpdates);
+  }
+  async afterInit() {
+    if (super.afterInit) {
+      super.afterInit();
+    }
+    if (!fs.existsSync(this.env.updatelog)) {
+      fs.writeFileSync(this.env.updatelog, '[]');
+    }
+    const executedUpdates = JSON.parse(fs.readFileSync(this.env.updatelog));
+    for (let i = 0; i < this.systemUpdates.length; i++) {
+      const update = this.systemUpdates[i];
+      const executed = executedUpdates.find(item => item.name === update.name);
+      if (executed) {
+        // eslint-disable-next-line no-console
+        console.log('Update', update.name, 'executed at', executed.executed);
+      } else {
+        // eslint-disable-next-line no-await-in-loop
+        await update.function();
+        // eslint-disable-next-line no-console
+        console.log('Executed ', update.name);
+        executedUpdates.push({ name: update.name, executed: new Date() });
+      }
+    }
+    fs.writeFileSync(this.env.updatelog, JSON.stringify(executedUpdates));
   }
 };
 AppServer.package = packageName;
---
+++ src/system-updates.js
@@ -0,0 +1,9 @@
+
+
+export const initUpdateSystem = async () => {
+  console.log('Test update');
+}
+
+export default [
+  { name: '2021-05-22 Init update system', function: initUpdateSystem },
+];
