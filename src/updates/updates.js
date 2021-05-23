7ee51679bacd4cdc6f2d1a618a67ed9883102f1a
Roman Nep <roman.nep@electroneek.com>
2021-05-23 19:30:07 +0400

    adjust chief role

--- src/updates/updates.js
+++ src/updates/updates.js
@@ -4,4 +4,5 @@ import { updateRoles } from './roles/updateRoles';
 
 export default [
   { name: '2021-05-23 Roles: extract tech support', function: updateRoles },
+  { name: '2021-05-23 Roles: adjust chief role', function: updateRoles },
 ];
