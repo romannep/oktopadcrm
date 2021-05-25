8203a71949098f327ad0f8486dd7b20d01315ff6
Roman Nep <roman.nep@electroneek.com>
2021-05-25 20:53:52 +0400

    move, resize and create events

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 import moment from 'moment';
 import { Calendar as BigCalendar, momentLocalizer } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import 'react-big-calendar/lib/addons/dragAndDrop/styles.scss'
+import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop';
 
 const localizer = momentLocalizer(moment);
+const BigCalendarDND = withDragAndDrop(BigCalendar);
 
 const containerStyle = {
   height: '80vh',
@@ -29,15 +32,83 @@ const messages = {
 };
 
 export default class Calendar extends Component {
+  state = {
+    events: [
+      {
+        start: moment().toDate(),
+        end: moment().add(2, 'hour').toDate(),
+        title: 'Some title',
+      },
+    ],
+  };
+
+  onEventResize = (data) => {
+    const { start, end } = data;
+
+    this.setState((state) => {
+      state.events[0].start = start;
+      state.events[0].end = end;
+      return { events: [...state.events] };
+    });
+  };
+
+  onEventDrop = (data) => {
+    console.log(data);
+  };
+
+  handleSelect = ({ start, end }) => {
+    const title = window.prompt('New Event name')
+    if (title)
+      this.setState({
+        events: [
+          ...this.state.events,
+          {
+            start,
+            end,
+            title,
+          },
+        ],
+      });
+  };
+
+  // handleDragStart = event => {
+  //   this.setState({ draggedEvent: event });
+  // };
+  //
+  // dragFromOutsideItem = () => {
+  //   return this.state.draggedEvent;
+  // };
+  //
+  // onDropFromOutside = ({ start, end, allDay }) => {
+  //   const { draggedEvent } = this.state;
+  //
+  //   const event = {
+  //     id: draggedEvent.id,
+  //     title: draggedEvent.title,
+  //     start,
+  //     end,
+  //     allDay: allDay,
+  //   };
+  //
+  //   this.setState({ draggedEvent: null })
+  //   this.moveEvent({ event, start, end })
+  // };
+
   render() {
     return (
       <div style={containerStyle}>
-        <BigCalendar
+        <BigCalendarDND
+          defaultDate={moment().toDate()}
+          defaultView='week'
+          events={this.state.events}
+          onEventDrop={this.onEventDrop}
+          onEventResize={this.onEventResize}
+          resizable
+          selectable
           localizer={localizer}
           messages={messages}
-          events={[]}
-          startAccessor="start"
-          endAccessor="end"
+          onSelectEvent={event => alert(event.title)}
+          onSelectSlot={this.handleSelect}
         />
       </div>
     );
